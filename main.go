f6deb8c047a9d711fd81ffe813aad62b8a0ae9c0
briheet01 <briheetyadav@gmail.com>
2023-12-17 19:13:09 +0530

    Added the port

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"log"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000" // Default to port 3000 if PORT is not set
+	}
+
 	store, err := NewPostgresStore()
 	if err != nil {
 		log.Fatal(err)
@@ -14,6 +20,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	server := NewAPIServer(":3000", store)
+	server := NewAPIServer(":"+port, store)
 	server.Run()
 }
